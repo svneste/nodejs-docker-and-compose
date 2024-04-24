0e5a004c4f7f6f22ce26dbc3616bcf79a230ea39
Sergei Nesterovich <sergeinesterovich@MacBook-Pro-Sergei.local>
2024-04-24 17:19:30 +0300

    deploy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:3333";
+export const URL = "https://api.kupi.nomoredomainswork.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
